14f12ad7ff3dd1628a93f8eca7b4362b7f78d6f3
Moyartu Manley <mmanley@macalester.edu>
2024-12-02 16:30:15 -0600

    Added more comments

--- src/Tiktok.java
+++ src/Tiktok.java
@@ -9,14 +9,28 @@ public class Tiktok implements Comparable<Tiktok>{
         this.playCount = playCount;
     }
 
+    /**
+     * 
+     * @return hashtags associated with a given Tiktok.
+     */
     public ArrayList<String> getHashtags(){
         return this.hashtags;
     }
 
+    /**
+     * 
+     * @return the playcount associated with a given Tiktok.
+     */
     public int getPlayCount(){
         return this.playCount;
     }
 
+    
+    /**
+     * Compares Tiktoks based on play count.
+     * @param other Tiktok
+     * @return 0 if tiktoks have the same play count, < 0 if this tiktok has a greater play count than the one being compared, > 0 if this tiktok has a less play count than the one being compared.
+     */
     @Override
     public int compareTo(Tiktok other) {
         return Integer.compare(this.playCount, other.playCount);
